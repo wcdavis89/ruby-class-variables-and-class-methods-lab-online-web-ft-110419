b5d41c622a1ea43e1e3600457e51cf4611e42421
Whittney Davis <wcdavis89@yahoo.com>
2019-11-21 05:04:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Book
   attr_accessor :artist, :name, :genre
   
-  @@song_count = 0 
+  @@count = 0 
   @@artists = []
   @@genres = []
 
@@ -9,11 +9,12 @@ class Book
     @name = name
     @artist = artists
     @genre = genre
-    @@song_count+=1 
+    @@count+=1 
     @@artists << artist
     @@genres << genre
   end
-
+  
+  def 
   
   def genre=(genre)
     @genre = genre
